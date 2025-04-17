058fc045721d6b5d05bf361f739405b85b3ac15e
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2025-04-17 15:00:23 +0530

    cors added

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import router from "./router.js"
 
 app.use("/api/create", router)
 
+const corsOption= {
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:["POST"],
+    allowHeaders:["Content-Type","Authorization"]
+}
 
-
+app.use(corsOption())
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
