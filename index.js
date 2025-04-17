af6fee9ead44db39db32552dc944511c871325c4
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2025-04-17 15:14:56 +0530

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = 8080
 app.use(express.json())
 import router from "./router.js"
 
-app.use("/api/create", router)
 
 const corsOptions = {
     origin: `http://localhost:5173`,
@@ -14,7 +13,8 @@ const corsOptions = {
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }
-
 app.use(cors(corsOptions))
+app.use("/api/create", router)
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
